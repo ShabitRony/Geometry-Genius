62133d8e3061d470bf1ead78f474698b348641ce
ShabitRony <shabitalahi123@gmail.com>
2025-01-16 19:10:19 +0600

    triangle part completed

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+function  calculateTriangleArea(){
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBase = triangleBaseInput.value;
+    const base = parseFloat(triangleBase)
+    // console.log(base);
+    
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeight = triangleHeightInput.value;
+    const height = parseFloat(triangleHeight)
+    // console.log(height);
+
+
+    const area = 0.5*base*height;
+    console.log(area);
+
+    const display = document.getElementById('show-calculation');
+    display.innerHTML = `The area of the triangle is: ${area}`;
+    
+}
